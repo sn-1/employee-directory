047d817f075e4bd90fc5f31d40d792184077a8c1
sanjida nawrin <sanjidanawrin123@gmail.com>
2020-07-02 22:50:34 -0400

    added src file

---
+++ src/App.test.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+const { getByText } = render(<App />);
+const linkElement = getByText(/learn react/i);
+expect(linkElement).toBeInTheDocument()});
+
+//import React from 'react';
+///import {render} from '@testing-library/react';
+//import App from './App';
+//import {isTSAnyKeyword} from '@babel/types';
+
+//it('renders without crashing', ()=>{
+ // const div = document.createElement('div');
+ // ReactDOM.render(<App />, div);
+ // ReactDOM.unmountComponentAtNode(div);
+//});
