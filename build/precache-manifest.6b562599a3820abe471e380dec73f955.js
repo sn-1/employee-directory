f0994e5d1808a668975e842fd01e28b6eb9b1b79
sanjida nawrin <sanjidanawrin123@gmail.com>
2020-07-04 11:25:23 -0400

    inisial commit

---
+++ build/precache-manifest.6b562599a3820abe471e380dec73f955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eaaa66a881cd81afb1e4dfd9dd40c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada04ba8a387b961d4d3",
+    "url": "/static/css/main.f745a164.chunk.css"
+  },
+  {
+    "revision": "962f1beecf1eeda3fb75",
+    "url": "/static/js/2.2cd49057.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2cd49057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada04ba8a387b961d4d3",
+    "url": "/static/js/main.34da2c13.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
