f035531932c12954b434e5af34ed58838b452241
sanjida nawrin <sanjidanawrin123@gmail.com>
2020-07-04 21:48:01 -0400

    app is working now

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,14 @@ test('renders learn react link', () => {
 const { getByText } = render(<App />);
 const linkElement = getByText(/learn react/i);
 expect(linkElement).toBeInTheDocument()});
+
+//import React from 'react';
+///import {render} from '@testing-library/react';
+//import App from './App';
+//import {isTSAnyKeyword} from '@babel/types';
+
+//it('renders without crashing', ()=>{
+ // const div = document.createElement('div');
+ // ReactDOM.render(<App />, div);
+ // ReactDOM.unmountComponentAtNode(div);
+//});
